df3ae7d58b88e084c644c698ecc071ef5af6fee1
Daniel Naranjo <>
2022-02-22 10:56:44 -0300

    updated

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-const { MongoClient } = require("mongodb");
+const { MongoClient, MongoServerError } = require("mongodb");
 
 const customers = [
 ];
 
+const dbCollection = "servers";
+
 const getDatabase = (values) => {
+    // mongodb url string
     let server = values.split("?")[0];
     let customer = server.slice(server.length - 7, server.length);
     return customer;
@@ -16,35 +19,35 @@ customers.forEach((customer) => {
   const dbName = getDatabase(customer);
 
   async function main() {
+    try {
+      await client.connect();
 
-    await client.connect();
-
-    console.log("Connected successfully to server");
-    const db = client.db(dbName);
-    const collection = db.collection("servers");
-
-    const filteredDocs = await collection.find({}).toArray();
-    console.log("Found document =>", filteredDocs);
-
-    const update2Doc = {
-        userName: "pix_gen_admin",
-        password: "kMzYkBG69YbUybkm",
-        storageUrl: "ftp://pixellot.ftp.upload.akamai.com/",
-        downloadUrl: "https://pixellot-vod.akamaized.net/pix_gen/{file}.{ext}",
-        streamingUrl: "https://pixellot-vod.akamaized.net/pix_gen/{file}.{ext}",
-        imageUrl: "https://pixellot-vod.akamaized.net/pix_gen/{file}.{ext}",
-        updated: new Date().getTime()
+      const db = client.db(dbName);
+      const collection = db.collection(dbCollection);
+  
+      const filteredDocs = await collection.find({}).toArray();
+  
+      const update2Doc = {
+          // fields goes here
+          updated: new Date().getTime()
+      }
+  
+      const updateResult = await collection.updateOne({}, { $set: update2Doc });
+      console.log('Updated document =>', updateResult);
+  
+      return `Connected successfully to server: ${dbName} -> ${filteredDocs.length}`;
+      
+    } catch(error) {
+      if (error instanceof MongoServerError) {
+        console.error("Error: ", dbName,' returns ',error.message);
+      } else {
+        throw error;
+      }
     }
-
-    //const updateResult = await collection.updateOne({}, { $set: update2Doc });
-    //console.log('Updated document =>', updateResult);
-
-    return "done.";
   }
 
   main()
     .then(console.log)
-    .catch(console.error)
     .finally(() => client.close());
 });
 
